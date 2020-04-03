e4416cc34bd58aca616ae8447d00410a8d770ef6
TakuCoder <thiyagu@programmer.net>
2020-04-03 11:46:09 +0530

    updates to deploy to heroku

--- src/app/pages/merchant/add-merchant/add-merchant.component.ts
+++ src/app/pages/merchant/add-merchant/add-merchant.component.ts
@@ -1,10 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { NbDateService } from '@nebular/theme';
+import { NgModule } from '@angular/core';
 
+import {
+  NbChatModule,
+  NbDatepickerModule,
+  NbDialogModule,
+  NbMenuModule,
+  NbSidebarModule,
+  NbToastrModule,
+  NbWindowModule,
+} from '@nebular/theme';
 @Component({
   selector: 'ngx-add-merchant',
   templateUrl: './add-merchant.component.html',
   styleUrls: ['./add-merchant.component.scss']
+})
+
+@NgModule
+(
+  {
+  declarations: [Component],
+  imports: [
+    NbChatModule,
+    NbDatepickerModule,
+    NbDialogModule,
+    NbMenuModule,
+    NbSidebarModule,
+    NbToastrModule,
+    NbWindowModule,
+  ],
+  bootstrap: [Component],
 })
 export class AddMerchantComponent {
 
